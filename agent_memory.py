45639c14957997d3cb510106c1e448f09080e8a6
Srinivas Kadira <srinivasusagml@gmail.com>
2025-06-20 19:24:59 -0400

    first commit

---
+++ agent_memory.py
@@ -0,0 +1,23 @@
+from smolagents import InferenceClientModel, CodeAgent
+from huggingface_hub import login
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env file
+load_dotenv()
+
+# Access the token
+token = os.getenv("HUGGINGFACE_TOKEN")
+
+print("Token loaded:", token[:5], "...")
+
+
+if not token:
+    raise ValueError("HUGGINGFACE_TOKEN environment variable not set. Please set it before running the script.")
+
+login(token=token)
+
+model = InferenceClientModel()
+agent = CodeAgent(tools=[], model=model, verbosity_level=0)
+
+result = agent.run("What's the 20th Fibonacci number?")
